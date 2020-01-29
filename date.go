007c3ba9810e8b366781500adc046ceffde644c3
joesri <j03sr1@gmail.com>
2020-01-29 17:30:00 +0100

    calendar: add setter and getter methods to type Event

--- date.go
+++ date.go
@@ -1,11 +1,26 @@
 // Package calendar defines a type Date.
 package calendar
 
-import "errors"
+import (
+  "errors"
+  "unicode/utf8"
+  )
 
 type Event struct {
-  Title string
+  title string
   Date
+}
+
+func (e *Event) Title() string {
+  return e.title
+}
+
+func (e *Event) SetTitle() error {
+  if utf8.RuneCountInString(title) > 30 {
+    return errors.New("invalid title")
+  }
+  e.title = title
+  return nil
 }
 
 type Date struct {
